54b565b779a41fdf108de89bf5c07a140c6feeee
xoyoc <xoyoc_l2@hotmail.com>
2024-11-11 20:58:20 -0600

    Creacion de modelos

--- equipo/models.py
+++ equipo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Equipo(models.Model):
+    placa = models.TextField(max_length=50)
+    modelo = models.TextField(max_length=100)
+    marca = models.TextField(max_length=100)
+    year = models.IntegerField(default=2023)
+    capacidad_tanque = models.IntegerField(default=10)
