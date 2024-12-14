c04e8b68951a49b39dd52b58a6bee8cd4516e79d
xoyoc <xoyoc_l2@hotmail.com>
2024-12-14 13:39:00 -0600

    Prueba de produccion

--- combustible/settings.py
+++ combustible/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT  = Path.joinpath(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
