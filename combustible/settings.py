aa1386841bac657a53fbd0fc51c3d27663e60f3a
xoyoc <xoyoc_l2@hotmail.com>
2025-10-13 23:26:48 -0600

    Correcion settings

--- combustible/settings.py
+++ combustible/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
